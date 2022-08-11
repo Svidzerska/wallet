88df4a7b4e0547b9fabe268b7f8dd9988c31182f
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-08-11 20:01:54 +0300

    server was redacted

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +29,16 @@ app.delete("/api/cards/:id", (req, res) => {
   cards = cards.filter((card) => card.id !== req.params.id);
   // res.send(`I received your DELETE request. Card with id ${req.params.id} was deleted`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
